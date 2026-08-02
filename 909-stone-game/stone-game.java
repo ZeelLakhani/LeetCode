2e592537303f443292e13f52acddcc39db33a8a2
ZeelLakhani <zeellakhani1883@gmail.com>
2026-08-02 16:18:56 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (62.03%) - LeetSync

--- 909-stone-game/stone-game.java
+++ 909-stone-game/stone-game.java
@@ -1,5 +1,22 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
-        return true;
+        int left = 0 , right = piles.length - 1;
+        int alice = 0 , bob = 0;
+        int turn = 0;
+
+        while (left < right) {
+            if (turn == 0) {
+                if (piles[left] < piles[right]) alice += piles[right--];
+                else alice += piles[left++];
+                turn = 1;
+            } else {
+                if (piles[left] < piles[right]) alice += piles[right--];
+                else alice += piles[left++];
+                turn = 0;
+            }
+        }
+        
+        return alice >= bob;
+        // return true;
     }
 }
