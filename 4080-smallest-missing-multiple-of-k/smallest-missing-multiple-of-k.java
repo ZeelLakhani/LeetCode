f8320ea0482e8b597b0374c913e92746423ba24a
ZeelLakhani <zeellakhani1883@gmail.com>
2026-08-25 17:08:37 +0530

    Time: 2 ms (68.20%) | Memory: 45.3 MB (52.43%) - LeetSync

--- 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.java
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.java
@@ -9,5 +9,16 @@ class Solution {
         while (set.contains(multiple)) multiple += k;
 
         return multiple;
+        
+        // List<Integer> list = new ArrayList<>();
+        // for (int i=1; i<=nums.length; i++) {
+        //     list.add(i);
+        // }
+
+        // for (int n : nums) {
+        //     if (n % k == 0) list.remove(Integer.valueOf(n/k));
+        // }
+        // if (list.size() == 0) return k * (nums.length + 1);
+        // return k * list.get(0);
     }
 }
